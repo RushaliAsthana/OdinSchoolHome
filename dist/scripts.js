058ac8f4b0d50a0582ca4ad199869f61917c1c93
Rushali Asthana <rushaliasthana@gmail.com>
2020-10-29 14:33:50 +0530

    added video modal page

--- dist/scripts.js
+++ dist/scripts.js
@@ -10,4 +10,38 @@ $(document).ready(function () {
 });
 
 
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+  $("iframe").each(function() { 
+      var src= $(this).attr('src');
+      $(this).attr('src',src);  
+});
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+ 
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+
+}
 
